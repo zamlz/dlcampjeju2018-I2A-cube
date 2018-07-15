120bd263c2b9214ecc4e4524985cdcce58346db4
Amlesh Sivanantham <samlesh@gmail.com>
2018-07-16 00:57:53 +0900

    More hyperparameter tuning

--- policy.py
+++ policy.py
@@ -88,8 +88,8 @@ class CnnPolicy(object):
     def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, reuse=False, coordConv=True):
         
         self.sess = sess
-        hid_size = 512
-        num_hid_layers = 4
+        hid_size = 1024
+        num_hid_layers = 0
 
         nw, nh, nc = ob_space
 
@@ -110,20 +110,21 @@ class CnnPolicy(object):
                 crd_tiled = tf.tile(crd, tf.stack([x_batch_size, 1, 1, 1]))
                 X = tf.concat([X, crd_tiled], axis=3)
 
-            conv1 = tf.layers.conv2d(activation=tf.nn.relu,
+            conv = tf.layers.conv2d(activation=tf.nn.relu,
                                         inputs=X,
                                         filters=16,
                                         kernel_size=[3,3],
                                         strides=[1,1],
                                         padding='VALID')
-            conv2 = tf.layers.conv2d(activation=tf.nn.relu,
-                                        inputs=conv1,
-                                        filters=16,
-                                        kernel_size=[3,3],
-                                        strides=[2,2],
-                                        padding='VALID')
+            #conv = tf.layers.conv2d(activation=tf.nn.relu,
+            #                            inputs=conv,
+            #                            filters=16,
+            #                            kernel_size=[3,3],
+            #                            strides=[2,2],
+            #                            padding='VALID')
 
-            h = tf.layers.dense(tf.layers.flatten(conv2), 4096, activation=tf.nn.tanh)
+            h = conv
+            h = tf.layers.dense(tf.layers.flatten(h), 4096, activation=tf.nn.tanh)
             h = tf.layers.dense(tf.layers.flatten(h), 2048, activation=tf.nn.tanh)
 
             with tf.variable_scope('pi'):
