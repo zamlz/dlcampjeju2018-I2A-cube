940f3638fb88578b861a97c2d65e1e0844e68e07
Amlesh Sivanantham <samlesh@gmail.com>
2018-07-20 19:19:42 +0900

    Some actor critic reorganization

--- a2c/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from a2c.trainer import train, ActorCritic
-from a2c.pd_test import pd_test
---
+++ actor_critic/__init__.py
@@ -0,0 +1,4 @@
+
+from actor_critic.trainer import train, ActorCritic
+from actor_critic.pd_test import pd_test
+from actor_critic.policy  import PolicyBuilder, policy_parser
